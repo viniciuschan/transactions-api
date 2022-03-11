adcfa1d677aa31b42737a6e2d64826df3e1ccd06
Vinicius Chan <vinicius.chan@olist.com>
2022-03-11 18:31:45 -0300

    add django_heroku

--- transactions_api/settings.py
+++ transactions_api/settings.py
@@ -1,8 +1,11 @@
 import os
 from pathlib import Path
 
+import django_heroku
 from decouple import config
 from dj_database_url import parse as parse_db_url
+
+django_heroku.settings(locals())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
